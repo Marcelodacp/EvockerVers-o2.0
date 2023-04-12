bfbf4aa36a04b16b4ffe527989104f6e0ed899a0
leoxinelao <leohrodriguesm@gmail.com>
2023-04-12 16:40:26 -0300

    Quase pronto para entregar

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ function proximaImg(){
 
     document.getElementById('radio'+cont).checked = true
 }
+document.querySelector(".hamburger").addEventListener("click", () =>
+    document.querySelector(".menu-hamburguer").classList.toggle("show-menu")
+);
+const usuariobtn = document.getElementById("loginBtn");
+const loginPopup = document.getElementById("loginPopup");
+const closeBtn = document.getElementById("closeBtn");
+
+loginBtn.addEventListener("click", () => {
+	loginPopup.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+	loginPopup.style.display = "none";
+});
