e576ff3a7736721c2db5c7ce8e70e8b4b1074606
leoxinelao <leohrodriguesm@gmail.com>
2023-05-03 16:41:18 -0300

    Bootstrap

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ loginBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
 	loginPopup.style.display = "none";
 });
+/* filtro galeria */
+$('.filter-btn').on('click', function() {
+
+    let type = $(this).attr('id');
+    let boxes = $('.gallery-items');
+
+
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    
+    if(type == 'rpg-btn') {
+        eachBoxes('rpg', boxes);
+      } else if(type == 'ave-btn') {
+        eachBoxes('ave', boxes);
+      } else if(type == 'his-btn') {
+        eachBoxes('his', boxes);
+      } else {
+        eachBoxes('all', boxes);
+      }
+  
+    });
+  
+    function eachBoxes(type, boxes) {
+  
+      if(type == 'all') {
+        $(boxes).fadeIn();
+      } else {
+        $(boxes).each(function() {
+          if(!$(this).hasClass(type)) {
+            $(this).fadeOut('slow');
+          } else {
+            $(this).fadeIn();
+          }
+        });
+      }
+    }
+  
